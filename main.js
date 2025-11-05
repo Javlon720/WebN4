7f7f33a201e82707596a4975cbb91bf0e0c46290
Javlon <abdullayevjavlon720@gmail.com>
2025-11-06 01:16:22 +0500

    started

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const Api='https://api.alquran.cloud/v1'
+const AUDIO_EDITION='ar.alafasy'
+const TEXT_EDITION='quran-simple'
+
+async function name(params) {
+    fetch.a
+    
+}
+
+function render() {
+    .map((item) => `  <div class="surah-card">
+                    <h3></h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>`)
+}
